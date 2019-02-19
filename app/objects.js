56f7b5995a58f5e19b736b79ca01a9120e55ccc1
huansern <aston@outlook.com>
2019-02-19 15:29:41 +0800

    solution: complete objects.js assessment

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    let result = [];
+    for(let [key, value] of Object.entries(obj)) {
+        if(obj.hasOwnProperty(key)) {
+            result.push(key + ': ' + value);
+        }
+    }
+    return result;
   }
 };
