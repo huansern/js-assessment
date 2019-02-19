ca03b6601837b8eb5ed42dd1af7e692c50ec6ec5
huansern <aston@outlook.com>
2019-02-19 15:28:01 +0800

    solution: complete count.js assessment

--- app/count.js
+++ app/count.js
@@ -2,6 +2,17 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    let counter = {
+        id: 0,
+        count: () => {
+            console.log(start++);
+            if(start <= end) {
+                counter.id = setTimeout(counter.count, 100)
+            }
+            return counter;
+        },
+        cancel: () => clearTimeout(counter.id)
+    };
+    return counter.count();
   }
 };
